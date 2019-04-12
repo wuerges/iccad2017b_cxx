163bea875e2e0c7ad5ad34c61887784b27d7c539
Emilio Wuerges <wuerges@gmail.com>
2019-04-12 12:30:40 -0300

    creating random shapes and querying them.

--- test/test_treap.cxx
+++ test/test_treap.cxx
@@ -50,13 +50,6 @@ int test_treap(const vector<Shape> & shapes)
 
 int main(int n, char**argv) {
 
-  rc::check("double reversal yields the original value",
-              [](const std::vector<int> &l0) {
-                auto l1 = l0;
-                std::reverse(begin(l1), end(l1));
-                std::reverse(begin(l1), end(l1));
-                RC_ASSERT(l0 == l1);
-              });
   rc::check("Check that added shapes can be queried",
             [](const std::vector<Shape> &shapes) {
               test_treap(shapes);
