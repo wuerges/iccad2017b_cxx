76e859d87c17160368655de24ab91ffde0b7f060
aluno-uffs <Aluno UFFS>
2019-04-15 17:25:35 -0300

    measuring execution time for input files.

--- test/test_parse.cxx
+++ test/test_parse.cxx
@@ -5,22 +5,36 @@
 #include <vector>
 #include <random>
 #include <algorithm>
+#include <chrono>
 
 using namespace std;
+using namespace std::chrono;
 using namespace iccad;
 
-int test_treap(const Input & inp)
+size_t count_shapes(const Input & inp) {
+  size_t c = 0;
+  for (int i = 0; i < inp.shapes.size(); i++) {
+      for(auto & v : inp.shapes[i]) {
+        ++c;
+      }
+  }
+  return c;
+}
+
+int test_treap(size_t slice, const Input & inp)
 {
 
     vector<Shape> shapes;
 
+    size_t slice_count = 0;
+
     for (int i = 0; i < inp.shapes.size(); i++) {
-        int layer = i*inp.viaCost;
+      if(slice_count >= slice) break;
+      int layer = i*inp.viaCost;
         for(auto & v : inp.shapes[i]) {
+          if(slice_count >= slice) break;
           shapes.push_back(Shape(PT(v[0], v[1], layer), PT(v[2], v[3], layer)));
-          // cout << "back: " << shapes.back() << '\n';
-          // cout << "a: " << PT(v[0], v[1], layer) << " b: " << PT(v[2], v[3], layer) << "\n";
-          // cout << "shape:" << Shape(PT(v[0], v[1], layer), PT(v[2], v[3], layer)) << "\n\n";
+          ++slice_count;
         }
     }
 
@@ -29,81 +43,57 @@ int test_treap(const Input & inp)
       root->add(shapes[i]);
     }
 
-
-    cout << root->count << '\n';
-    // root->print();
-
+    int result = 0;
     for(auto s : shapes) {
-      int result = 100000;
-      result = root->query(s.a, s.b);
-      cout << result << " ";
+      result += root->query(s.a, s.b);
     }
-    cout << "\n";
+    // cout << result << "\n";
 
-    // for (int i = 0; i < inp.shapes.size(); i++) {
-    //     int layer = i*inp.viaCost;
-    //     for(auto & v : inp.shapes[i]) {
-    //         Shape shape{
-    //                 PT{v[0], v[1], layer},
-    //                 PT{v[2], v[3], layer}};
-    //
-    //         int result = 100000;
-    //         int xsize = inp.boundary[2] - inp.boundary[0];
-    //         int ysize = inp.boundary[3] - inp.boundary[1];
-    //         int zsize = inp.n_vias*inp.viaCost;
-    //         while(result > 1000) {
-    //             result = root->query(shape.a, shape.b);
-    //         }
-    //         cout << result << " ";
-    //     }
-    // }
-    // cout << "\n";
-
-    //
-    // for(int a = 0; a < 10; ++a) {
-    //     for(int b = a; b < 10; ++b) {
-    //         //int q = root->query({a, b, 0});
-    //         //cout << "query "<<a<<","<<b<<" = " << q << "\n\n";
-    //     }
-    // }
-    return 0;
+    return result;
 }
 
 
 int main(int n, char**argv) {
 
-  std::cout << "Parsing file " << argv[1] << '\n';
+  // std::cout << "Parsing file " << argv[1] << '\n';
 
   Input i;
   // try {
-    parser::parse_file(i, argv[1]);
+    parser::parse_file(i, argv[2]);
+
+    int slice = atoi(argv[1]);
+
   // }
   // catch (...) {
   //   std::cerr << "ERROR: PARSE FAILED\n";
   // }
 
-  std::cout << "Via Cost: " << i.viaCost << '\n';
-  std::cout << "sp: " << i.spacing << '\n';
-  std::cout << "boundary:" << i.boundary[0] << ' '
-	  << i.boundary[1] << ' '
-	  << i.boundary[2] << ' '
-	  << i.boundary[3] << '\n';
-  std::cout << i.shapes.size() << '\n';
-  std::cout << i.vias.size() << '\n';
-  std::cout << i.obstacles.size() << '\n';
-
+  // std::cout << "Via Cost: " << i.viaCost << '\n';
+  // std::cout << "sp: " << i.spacing << '\n';
+  // std::cout << "boundary:" << i.boundary[0] << ' '
+	//   << i.boundary[1] << ' '
+	//   << i.boundary[2] << ' '
+	//   << i.boundary[3] << '\n';
+  // std::cout << i.shapes.size() << '\n';
+  // std::cout << i.vias.size() << '\n';
+  // std::cout << i.obstacles.size() << '\n';
 
   shuffle(i.shapes.begin(), i.shapes.end(), std::default_random_engine(0));
-
 
   // Grid g;
   // convert(i, g);
   // generate_hannan(g);
 
+  size_t n_shapes = count_shapes(i);
 
-  test_treap(i);
 
-
+  for(int slice = 100; slice < n_shapes; slice+= 500) {
+    auto t1 = std::chrono::high_resolution_clock::now();
+    size_t result = test_treap(slice, i);
+    auto t2 = std::chrono::high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>( t2 - t1 ).count();
+    cout << slice << ", " << duration <<","<< result<<'\n';
+  }
 
   return 0;
 }
