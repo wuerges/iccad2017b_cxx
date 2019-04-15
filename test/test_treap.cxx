4f07de75d672701b624e21dbf6117ed9d82c1d46
aluno-uffs <Aluno UFFS>
2019-04-15 13:47:41 -0300

    improved test generation

--- test/test_treap.cxx
+++ test/test_treap.cxx
@@ -13,20 +13,23 @@ using namespace iccad;
 
 namespace rc {
 
+  const auto genPT = gen::container<array<int, 3>>(gen::inRange(0, 100000));
+
   template<>
   struct Arbitrary<PT> {
     static Gen<PT> arbitrary() {
-      return gen::map(gen::arbitrary<array<int, 3>>(),
+      return gen::map(genPT,
         [](array<int, 3> vs) {
           return PT(vs[0], vs[1], vs[2]);
         });
     }
   };
 
+
   template<>
   struct Arbitrary<Shape> {
     static Gen<Shape> arbitrary() {
-      return gen::map(gen::arbitrary<array<array<int, 3>, 2>>(),
+      return gen::map(gen::container<array<array<int, 3>, 2>>(genPT),
         [](array<array<int, 3>, 2> pts) {
           return Shape(
             PT(min(pts[0][0], pts[1][0]), min(pts[0][1], pts[1][1]), min(pts[0][2], pts[1][2])),
