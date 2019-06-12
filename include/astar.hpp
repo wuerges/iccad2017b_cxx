ed2b4caddb710ba19324f4ca4b2da3123c6f6ba0
Emilio Wuerges <wuerges@gmail.com>
2019-06-12 15:51:23 -0300

    made some astar members private

--- include/astar.hpp
+++ include/astar.hpp
@@ -21,24 +21,21 @@ struct AStar {
 
         AStar(Treap & sh, Treap & obs, const Shape & s1, const Shape & s2, V1D b);
 
-        void add_shape(const Shape & s) ;
 
-        vector<index> neighboors(index i);
-
-        const PT make_pt(index i);
-
-        int find(int c, const vector<int> & v) const ;
-
-        index find(const PT p) const;
-
-        void remove_duplicates(vector<int> & v) ;
-
-        void fix_boundaries(vector<int> & v, int min_bound, int max_bound);
 
         vector<PT> run(const Shape & s, const Shape & t) ;
 
+
+private:
+        vector<index> neighboors(index i);
+        const PT make_pt(index i);
+        int find(int c, const vector<int> & v) const ;
+        index find(const PT p) const;
+
+        void remove_duplicates(vector<int> & v) ;
+        void fix_boundaries(vector<int> & v, int min_bound, int max_bound);
+        void add_shape(const Shape & s) ;
         vector<PT> bad_run(const PT s, const Shape & ts) ;
-        
         vector<PT> run1(const Shape & shape_s, const Shape & shape_t) ;
     };
 
