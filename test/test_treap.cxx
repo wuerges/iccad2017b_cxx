bd92560855a254256aaa1605d1f32e7833dff881
Emilio Wuerges <wuerges@gmail.com>
2019-06-14 15:50:16 -0300

    moved test generators to generators.hpp

--- test/test_treap.cxx
+++ test/test_treap.cxx
@@ -8,39 +8,11 @@
 #include <algorithm>
 #include <set>
 
+#include "generators.hpp"
+
 using namespace std;
 using namespace iccad;
 
-
-namespace rc {
-
-  const auto genPT = gen::container<array<int, 3>>(gen::inRange(0, 100000));
-
-  template<>
-  struct Arbitrary<PT> {
-    static Gen<PT> arbitrary() {
-      return gen::map(genPT,
-        [](array<int, 3> vs) {
-          return PT(vs[0], vs[1], 0);
-        });
-    }
-  };
-
-
-  template<>
-  struct Arbitrary<Shape> {
-    static Gen<Shape> arbitrary() {
-      return gen::map(gen::container<array<array<int, 3>, 2>>(genPT),
-        [](array<array<int, 3>, 2> pts) {
-          return Shape(
-            PT(min(pts[0][0], pts[1][0]), min(pts[0][1], pts[1][1]), 0 ), // min(pts[0][2], pts[1][2])),
-            PT(max(pts[0][0]+1, pts[1][0]), max(pts[0][1]+1, pts[1][1]), 0 ) //max(pts[0][2], pts[1][2]))
-          );
-        });
-    }
-  };
-
-}
 
 int test_treap(const vector<Shape> & shapes)
 {
