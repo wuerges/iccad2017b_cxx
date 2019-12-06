69169c0a9c0da2f735295d189e10bab555b707b2
Emilio Wuerges <wuerges@gmail.com>
2019-12-06 11:53:34 -0300

    fixing identation

--- lib/mst_iterative_3step.cxx
+++ lib/mst_iterative_3step.cxx
@@ -20,23 +20,22 @@ struct Edge {
     Edge(const Shape  *c, const RTree & t): u(c), queue(new RTreeQueue(*c, t)) {}
 
     friend ostream & operator<<(ostream& out, const Edge& e) {
-      if(e.queue) {
-        out << "EdgeQueue{" << e.queue->center << ","<< e.queue->peek() << "}";
-      }
-      else {
-        out << "Edge{" << *e.u <<","<< *e.v<< "}";
-      }
-      return out;
+        if(e.queue) {
+            out << "EdgeQueue{" << e.queue->center << ","<< e.queue->peek() << "}";
+        }
+        else {
+            out << "Edge{" << *e.u <<","<< *e.v<< "}";
+        }
+        return out;
     }
 };
 
 template<>
 vector<Route> MST::run_mst<ITERATIVE_MST_3STEP>(
-//     const Treap & treap, 
-//   const Treap & obstacles,
   const vector<Shape> & shapes, 
   const vector<Shape> & obs_vector, 
-  const V1D & boundary) {
+  const V1D & boundary) 
+{
     RTree treap, obstacles;
     treap.populate(shapes);
     obstacles.populate(obs_vector);
@@ -53,17 +52,17 @@ vector<Route> MST::run_mst<ITERATIVE_MST_3STEP>(
         unique_ptr<Edge> e(new Edge(&o, treap));
         int rem = 0;
         while(!e->queue->empty() && e->queue->peek() == 0) {
-          auto v = e->queue->pop();
-          if (muf.Find(&o) != muf.Find(v)) {
-            muf.Union(&o, v);
-            connected++;
-          }
-          rem++;
+            auto v = e->queue->pop();
+            if (muf.Find(&o) != muf.Find(v)) {
+                muf.Union(&o, v);
+                connected++;
+            }
+            rem++;
         }
 
         for(int i = 0; i < LOCAL_NEIGHBOORS && !e->queue->empty(); ++i) {
-          const auto v = e->queue->pop();
-          krusk.emplace(distance(o, *v), new Edge(&o, v));
+            const auto v = e->queue->pop();
+            krusk.emplace(distance(o, *v), new Edge(&o, v));
         }  
         int new_key = e->queue->peek();
         krusk.emplace(new_key, std::move(e));
@@ -79,62 +78,75 @@ vector<Route> MST::run_mst<ITERATIVE_MST_3STEP>(
         krusk.erase(krusk.begin());
 
         if(work->queue) {
-          // printf("work->queue()\n");
+            // printf("work->queue()\n");
 
-          if(!work->queue->empty()) {
-            const auto u = work->u;
-            const auto v = work->queue->pop();
-            int new_key = work->queue->peek();
+            if(!work->queue->empty()) {
+                const auto u = work->u;
+                const auto v = work->queue->pop();
+                int new_key = work->queue->peek();
 
-            const Shape * mu = muf.Find(u);
-            const Shape * mv = muf.Find(v);
-            if(mu != mv) { 
-              // adds new edge
-              unique_ptr<Edge> routed(new Edge(u,v));
-              routed->route = local_route_step_2(obstacles, *u, *v, boundary);
-              routed->step++;
+                const Shape * mu = muf.Find(u);
+                const Shape * mv = muf.Find(v);
+                if(mu != mv) { 
+                    // adds new edge
+                    unique_ptr<Edge> routed(new Edge(u,v));
+                    routed->route = local_route_step_2(obstacles, *u, *v, boundary);
+                    routed->step++;
 
-              int route_length = routed->route->length();
-              if(route_length == old_length) {
-                muf.Union(u, v);
-                result.push_back(*routed->route);
-                connected++;
-              }
-              else {
-                krusk.emplace(route_length, std::move(routed));
-              }
+                    int route_length = routed->route->length();
+                    if(route_length == old_length) {
+                        muf.Union(u, v);
+                        result.push_back(*routed->route);
+                        connected++;
+                    }
+                    else {
+                        krusk.emplace(route_length, std::move(routed));
+                    }
+                }
+
+                krusk.emplace(new_key, std::move(work));
             }
-
-            krusk.emplace(new_key, std::move(work));
-          }
         }
         else {
-          const Shape *mu = muf.Find(work->u);
-          const Shape *mv = muf.Find(work->v);
-          const Shape *u = work->u;
-          const Shape *v = work->v;
-            
-          if(mu != mv) {
-              if (work->step == 0) {
-                  work->route = local_route_step_2(obstacles, *u, *v, boundary);
-                  work->step++;
-                  int route_length = work->route->length();
-                  if(route_length == old_length) {
+            const Shape *mu = muf.Find(work->u);
+            const Shape *mv = muf.Find(work->v);
+            const Shape *u = work->u;
+            const Shape *v = work->v;
+                
+            if(mu != mv) {
+                if (work->step == 0) {
+                    work->route = local_route_step_2(obstacles, *u, *v, boundary);
+                    work->step++;
+                    int route_length = work->route->length();
+                    if(route_length == old_length) {
+                        muf.Union(u, v);
+                        result.push_back(*work->route);
+                        connected++;
+                    }
+                    else {
+                        krusk.emplace(route_length, std::move(work));
+                    }
+
+                }
+                else if (work->step == 1) {
+                    work->route = local_route_step_2(obstacles, *u, *v, boundary);
+                    work->step++;
+                    int route_length = work->route->length();
+                    if(route_length == old_length) {
+                        muf.Union(u, v);
+                        result.push_back(*work->route);
+                        connected++;
+                    }
+                    else {
+                        krusk.emplace(route_length, std::move(work));
+                    }
+                }
+                else {
                     muf.Union(u, v);
                     result.push_back(*work->route);
                     connected++;
-                  }
-                  else {
-                    krusk.emplace(route_length, std::move(work));
-                  }
-
-              }
-              else {
-                  muf.Union(u, v);
-                  result.push_back(*work->route);
-                  connected++;
-              }            
-          }
+                }            
+            }
         }
     }
 
